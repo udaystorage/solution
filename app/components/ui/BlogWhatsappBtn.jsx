850b3d156ce9380e65fd2c6f9f764d962c8ad56c
Somnath Baidya <somnathbaidyahere@gmail.com>
2026-07-19 13:01:44 +0530

    changed the copy of the talk to expert btn in blog aside comp

--- app/components/ui/BlogWhatsappBtn.jsx
+++ app/components/ui/BlogWhatsappBtn.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { openWhatsApp } from "@/lib/whatsapp";
+import { ArrowRight } from "lucide-react";
 
 
 function BlogWhatsappBtn() {
@@ -10,9 +11,11 @@ function BlogWhatsappBtn() {
           "Hi, I would like to talk to the back-office expert team.",
         )
       }
-      className="w-full bg-stone-950 text-stone-100 hover:bg-stone-900 hover:text-white hover:scale-105 transition-transform duration-300 ease-in-out hover:scale-105 transition-colors cursor-pointer  py-2.5 px-4 text-xs font-medium tracking-wide rounded-xl"
-    >
-      Consult With Our Expert
+className="w-full flex items-center justify-evenly bg-stone-950 text-stone-100 hover:bg-stone-800
+hover:-translate-y-0.5 hover:shadow-[0_8px_25px_rgba(0,0,0,.25)]
+transition-all duration-300 cursor-pointer py-2.5 px-4
+text-xs font-medium tracking-wide rounded-xl"    >
+      Talk to an Expert <ArrowRight width={15}/>
     </button>
   );
 }
