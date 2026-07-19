810a3987e97d309adcd955ac031356be2227e819
Somnath Baidya <somnathbaidyahere@gmail.com>
2026-07-19 13:05:47 +0530

    improved btn text arrow alignments

--- app/components/ui/BlogWhatsappBtn.jsx
+++ app/components/ui/BlogWhatsappBtn.jsx
@@ -11,7 +11,7 @@ function BlogWhatsappBtn() {
           "Hi, I would like to talk to the back-office expert team.",
         )
       }
-className="w-full flex items-center justify-evenly bg-stone-950 text-stone-100 hover:bg-stone-800
+className="w-full flex items-center justify-center gap-4 bg-stone-950 text-stone-100 hover:bg-stone-800
 hover:-translate-y-0.5 hover:shadow-[0_8px_25px_rgba(0,0,0,.25)]
 transition-all duration-300 cursor-pointer py-2.5 px-4
 text-xs font-medium tracking-wide rounded-xl"    >
