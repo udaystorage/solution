e101d31f8aa159006fd8efcbadf04251f2deea55
Somnath Baidya <somnathbaidyahere@gmail.com>
2026-07-20 12:04:24 +0530

    Split into component based architecture

--- app/aboutus/page.js
+++ app/aboutus/page.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 
 function page() {
@@ -7,3 +8,45 @@ function page() {
 }
 
 export default page
+=======
+"use client";
+
+import { useState } from "react";
+import {
+  ArrowDown,
+  ArrowRight,
+  ArrowUpRight,
+  Check,
+  Database,
+  Search,
+  ShieldCheck,
+  SlidersHorizontal,
+  PackageCheck,
+  Cpu,
+  Users,
+  MapPin,
+} from "lucide-react";
+import Image from "next/image";
+
+import AboutHero from "../components/sections/aboutPage/AboutHero";
+import WhyWeExist from "../components/sections/aboutPage/WhyWeExist";
+import OurApproach from "../components/sections/aboutPage/OurApproach";
+import OurPrinciples from "../components/sections/aboutPage/OurPrinciples";
+import HumanExpertise from "../components/sections/aboutPage/HumanExpertise";
+import FinalCTA from "../components/sections/aboutPage/FinalCTA";
+import ReachCapabilities from "../components/sections/aboutPage/ReachCapabilities";
+
+export default function AboutPage() {
+  return (
+    <main className="overflow-hidden bg-white text-stone-950">
+      <AboutHero />
+      <WhyWeExist />
+      <OurApproach />
+      <OurPrinciples />
+      <ReachCapabilities />
+      <HumanExpertise />
+      <FinalCTA />
+    </main>
+  );
+}
+>>>>>>> 5c3acfc678e885bbc71748cef0ff6aa802ded47a
