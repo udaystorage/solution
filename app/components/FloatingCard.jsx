5f88d313d329be5f856452c3679b553299164d05
Somnath Baidya <somnathbaidyahere@gmail.com>
2026-07-22 13:42:09 +0530

    imrpoving flip card animation 2

--- app/components/FloatingCard.jsx
+++ app/components/FloatingCard.jsx
@@ -6,18 +6,32 @@ export default function FloatingCard() {
   return (
 <div
   className="
-    relative w-full
-    max-w-[420px]
-    sm:max-w-[520px]
-    md:max-w-[620px]
-    lg:max-w-[700px]
+    relative
+    w-full
     mx-auto
-    flex items-center justify-center
-    overflow-hidden
+    flex
+    items-center
+    justify-center
+    overflow-visible
+
+    max-w-[400px]
+
+    sm:max-w-[500px]
+
+    md:max-w-[600px]
+
+    lg:max-w-none
+    lg:w-[680px]
+    lg:-translate-x-4
+
+    xl:w-[720px]
+    xl:-translate-x-2
+
+    2xl:w-[760px]
+    2xl:translate-x-0
   "
->          {/* <div className="w-80 h-40 bg-white/40 rounded-2xl shadow-xl p-4">Card</div> */}
-          {/* <DustCard/> */}
-          <CardFlipAnimation/>
-    </div>
+>
+  <CardFlipAnimation />
+</div>
   );
 }
